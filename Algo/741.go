b37d36804141a40aa63d90aad4d06c8d89236a4e
gole_huang <gole_huang@sohu.com>
2022-07-13 00:38:23 +0800

    almost done

--- Algo/741.go
+++ Algo/741.go
@@ -1,39 +1,5 @@
 package Algo
 
-import "fmt"
-
-/*
-type farm struct {
-	grid   [][]int
-	length int
-}
-*/
-
-/*
-TODO:findPath(x,y) [][][2]int
-若cherry(x,y)==-1，则返回nil；
-否则若x==y==cherry.length-1，则返回[][][2]int{{{x,y}}}
-否则若x==cherry.length-1，则在findPath(x,y+1)的每一个切片后添加(x,y);
-	pathY := findPath(x,y+1)
-	for i, _ := range pathY {
-		pathY[i] = append(pathY[i],{x,y})
-	}
-否则若y==cherry.length-1，则在findPath(x+1,y)的每一个切片后添加(x,y);
-	pathX := findPath(x+1,y)
-	for i, _ := range pathX {
-		pathX[i] = append(pathX[i],{x,y})
-	}
-否则添加findPath(x,y+1)，(x,y)和findPath(x+1,y)，(x,y);
-	pathX := findPath(x+1,y)
-	for i, _ := range pathX {
-		pathX[i] = append(pathX[i],{x,y})
-	}
-	pathY := findPath(x,y+1)
-	for i, _ := range pathY {
-		pathY[i] = append(pathY[i],{x,y})
-	}
-	paths = append(pathX, pathY...)
-*/
 func findPath(x, y int, cherry [][]int) [][][2]int {
 	length := len(cherry)
 	if cherry[x][y] == -1 {
@@ -44,7 +10,7 @@ func findPath(x, y int, cherry [][]int) [][][2]int {
 	} else {
 		var pathX, pathY [][][2]int
 		if x != length-1 {
-			pathX := findPath(x+1, y, cherry)
+			pathX = findPath(x+1, y, cherry)
 			if pathX != nil {
 				for i := range pathX {
 					pathX[i] = append(pathX[i], [2]int{x, y})
@@ -52,7 +18,7 @@ func findPath(x, y int, cherry [][]int) [][][2]int {
 			}
 		}
 		if y != length-1 {
-			pathY := findPath(x, y+1, cherry)
+			pathY = findPath(x, y+1, cherry)
 			if pathY != nil {
 				for i := range pathY {
 					pathY[i] = append(pathY[i], [2]int{x, y})
@@ -88,7 +54,6 @@ func pickMost(x, y int, cherry [][]int) int {
 			xMove := pickMost(x+1, y, cherry)
 			yMove := pickMost(x, y+1, cherry)
 			if xMove == -1 && yMove == -1 {
-				fmt.Printf("grid[%d][%d] back to %d\n", x, y, res)
 				return -1
 			} else if xMove > yMove {
 				return res + xMove
@@ -99,22 +64,20 @@ func pickMost(x, y int, cherry [][]int) int {
 	}
 }
 
-func CherryPickup(Cherry [][]int) int {
-	//length := len(Cherry)
-	fmt.Printf("Begin grid: %v\n", Cherry)
-	//计算每一条路径中，第一次和第二次采集樱桃的总数
+func CherryPickup(cherry [][]int) int {
 	total := 0
-	for _, path := range findPath(0, 0, Cherry) {
+	for _, path := range findPath(0, 0, cherry) {
 		first := 0
-		cherry := Cherry
-		fmt.Printf("Before grid: %v\n", cherry)
-		for i := 0; i < len(path); i++ {
-			first += cherry[path[i][0]][path[i][1]]
-			cherry[path[i][0]][path[i][1]] = 0
+		grid := make([][]int, len(cherry))
+		for i := 0; i < len(cherry); i++ {
+			grid[i] = make([]int, len(cherry[i]))
+			copy(grid[i], cherry[i])
 		}
-		fmt.Printf("After grid: %v\n", cherry)
-		second := pickMost(0, 0, cherry)
-		fmt.Printf("first: %v\nSecond: %v\n", first, second)
+		for i := 0; i < len(path); i++ {
+			first += grid[path[i][0]][path[i][1]]
+			grid[path[i][0]][path[i][1]] = 0
+		}
+		second := pickMost(0, 0, grid)
 		if total < first+second {
 			total = first + second
 		}
