a310af89149d71c453fc23a656c44aed0e531acb
gole_huang <gole_huang@sohu.com>
2022-07-15 17:42:59 +0800

    cut some path but useless

--- Algo/741.go
+++ Algo/741.go
@@ -1,30 +1,30 @@
 package Algo
 
-func findPath(x, y int, cherry [][]int) []map[[2]int]int {
+import "fmt"
+
+func firstPath(x, y int, cherry [][]int) [][][2]int {
+	//fmt.Printf("(%v,%v\n", x, y)
 	length := len(cherry)
 	if cherry[x][y] == -1 {
 		return nil
 	}
 	if x == length-1 && y == length-1 {
-		pathMap := make([]map[[2]int]int, 1)
-		pathMap[0] = make(map[[2]int]int, 1)
-		pathMap[0][[2]int{x, y}] = cherry[x][y]
-		return pathMap
+		return [][][2]int{{{x, y}}}
 	} else {
-		var pathX, pathY []map[[2]int]int
+		var pathX, pathY [][][2]int
 		if x != length-1 {
-			pathX = findPath(x+1, y, cherry)
+			pathX = firstPath(x+1, y, cherry)
 			if pathX != nil {
-				for i := range pathX {
-					pathX[i][[2]int{x, y}] = cherry[x][y]
+				for i := 0; i < len(pathX); i++ {
+					pathX[i] = append(pathX[i], [2]int{x, y})
 				}
 			}
 		}
 		if y != length-1 {
-			pathY = findPath(x, y+1, cherry)
+			pathY = firstPath(x, y+1, cherry)
 			if pathY != nil {
-				for i := range pathY {
-					pathY[i][[2]int{x, y}] = cherry[x][y]
+				for i := 0; i < len(pathY); i++ {
+					pathY[i] = append(pathY[i], [2]int{x, y})
 				}
 			}
 		}
@@ -33,21 +33,23 @@ func findPath(x, y int, cherry [][]int) []map[[2]int]int {
 	}
 }
 
-func pickMost(x, y int, cherry [][]int, pickMap map[[2]int]int) int {
+func secondPath(x, y, target int, cherry [][]int) int {
 	length := len(cherry)
-	if cherry[x][y] == -1 {
+	if cherry[x][y] == -1 || target >= (length)*2-1-x-y {
 		return -1
 	} else {
-		res := cherry[x][y] - pickMap[[2]int{x, y}]
+		res := cherry[x][y]
+		target -= res
 		if x == length-1 && y == length-1 {
 			return res
 		} else {
-			var xMove, yMove int
+			//！！！！！必须要给予xMove/yMove初始值-1！！！！！
+			xMove, yMove := -1, -1
 			if x != length-1 {
-				xMove = pickMost(x+1, y, cherry, pickMap)
+				xMove = secondPath(x+1, y, target, cherry)
 			}
 			if y != length-1 {
-				yMove = pickMost(x, y+1, cherry, pickMap)
+				yMove = secondPath(x, y+1, target, cherry)
 			}
 			if xMove == -1 && yMove == -1 {
 				return -1
@@ -62,25 +64,24 @@ func pickMost(x, y int, cherry [][]int, pickMap map[[2]int]int) int {
 
 func CherryPickup(cherry [][]int) int {
 	total := 0
-	paths := findPath(0, 0, cherry)
-	for _, path := range paths {
+	fmt.Printf("Start\n")
+	paths := firstPath(0, 0, cherry)
+	fmt.Printf("Find all paths\n")
+	for p := 0; p < len(paths); p++ {
 		first := 0
 		grid := make([][]int, len(cherry))
 		for i := 0; i < len(cherry); i++ {
 			grid[i] = make([]int, len(cherry[i]))
 			copy(grid[i], cherry[i])
 		}
-		/*
-			for i := 0; i < len(path); i++ {
-				first += grid[path[i][0]][path[i][1]]
-				grid[path[i][0]][path[i][1]] = 0
-			}
-		*/
-		for p := range path {
-			first += cherry[p[0]][p[1]]
-			cherry[p[0]][p[1]] = 0
+		for i := 0; i < len(paths[p]); i++ {
+			first += grid[paths[p][i][0]][paths[p][i][1]]
+			grid[paths[p][i][0]][paths[p][i][1]] = 0
 		}
-		second := pickMost(0, 0, grid, path)
+		if first < total>>1 {
+			continue
+		}
+		second := secondPath(0, 0, total-first, grid)
 		if total < first+second {
 			total = first + second
 		}
