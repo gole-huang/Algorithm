025e362891bc4cbfc2af4cbb3296f9f9db2e7d5c
gole_huang <gole_huang@sohu.com>
2022-07-17 02:15:40 +0800

    cut more path but useless

--- Algo/741.go
+++ Algo/741.go
@@ -1,29 +1,92 @@
 package Algo
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func firstPath(x, y int, cherry [][]int) [][][2]int {
-	//fmt.Printf("(%v,%v\n", x, y)
-	length := len(cherry)
-	if cherry[x][y] == -1 {
-		return nil
+type Cherry struct {
+	Grid [][][2]bool
+}
+
+func (grid *Cherry) transform(cherry [][]int) {
+	grid.Grid = make([][][2]bool, len(cherry))
+	for i := 0; i < len(cherry); i++ {
+		grid.Grid[i] = make([][2]bool, len(cherry[i]))
+		for j := range cherry[i] {
+			if cherry[i][j] == -1 {
+				grid.Grid[i][j] = [2]bool{true, false}
+			} else if cherry[i][j] == 1 {
+				grid.Grid[i][j] = [2]bool{false, true}
+			} else {
+				grid.Grid[i][j] = [2]bool{false, false}
+			}
+		}
 	}
+}
+
+func (grid *Cherry) copyFrom(cherry *Cherry) {
+	grid.Grid = make([][][2]bool, len(cherry.Grid))
+	for i := 0; i < len(cherry.Grid); i++ {
+		grid.Grid[i] = make([][2]bool, len(cherry.Grid[i]))
+		for j := range cherry.Grid[i] {
+			grid.Grid[i][j] = cherry.Grid[i][j]
+		}
+	}
+}
+
+func firstPath(x, y int, cherry *Cherry) [][][2]int {
+	length := len(cherry.Grid)
+	var pathX, pathY [][][2]int
 	if x == length-1 && y == length-1 {
 		return [][][2]int{{{x, y}}}
+	} else if x == length-1 {
+		pathY = make([][][2]int, 1)
+		pathY[0] = make([][2]int, 0)
+		for i := y; i < length; i++ {
+			if cherry.Grid[x][i][0] {
+				return nil
+			} else {
+				pathY[0] = append(pathY[0], [2]int{x, i})
+			}
+		}
+		return pathY
+	} else if y == length-1 {
+		pathX = make([][][2]int, 1)
+		pathX[0] = make([][2]int, 0)
+		for i := x; i < length; i++ {
+			if cherry.Grid[i][y][0] {
+				return nil
+			} else {
+				pathX[0] = append(pathX[0], [2]int{i, y})
+			}
+		}
+		return pathX
 	} else {
-		var pathX, pathY [][][2]int
-		if x != length-1 {
+		//对于某一个节点，需要到达右下节点可通过2条路经，除非先右后下比先下后右大，否则都先下后右
+		/*当且仅当满足下述条件，需要添加下方路径集合：
+		**下方单元格不为-1，且
+		**没有左下方，或
+		***本单元格为1或左下方单元格为0*/
+		if !cherry.Grid[x+1][y][0] { //添加下方路径
 			pathX = firstPath(x+1, y, cherry)
-			if pathX != nil {
-				for i := 0; i < len(pathX); i++ {
+			for i := 0; i < len(pathX); i++ {
+				if !cherry.Grid[x+1][y][1] && cherry.Grid[x][y+1][1] && (pathX[i] == nil || pathX[i][len(pathX[i])-1] == [2]int{x + 1, y + 1}) { //当且仅当无左下节点，或者本节点为1或左下节点为0
+					pathX[i] = nil
+				} else if pathX[i] != nil {
 					pathX[i] = append(pathX[i], [2]int{x, y})
 				}
 			}
 		}
-		if y != length-1 {
+		/*当且仅当满足下述条件，需要添加右方路径集合：
+		**右方单元格不为-1，且
+		**没有右上方，或
+		***本单元格为1且右上方单元格为0*/
+		if !cherry.Grid[x][y+1][0] { //添加右方路径
 			pathY = firstPath(x, y+1, cherry)
-			if pathY != nil {
-				for i := 0; i < len(pathY); i++ {
+			for i := 0; i < len(pathY); i++ {
+				if (cherry.Grid[x+1][y][1] || !cherry.Grid[x][y+1][1]) && (pathY[i] == nil || pathY[i][len(pathY[i])-1] == [2]int{x + 1, y + 1}) { //当且仅当无右上节点，或者本节点为1且右上节点为0
+					pathY[i] = nil
+				} else if pathY[i] != nil {
 					pathY[i] = append(pathY[i], [2]int{x, y})
 				}
 			}
@@ -33,22 +96,27 @@ func firstPath(x, y int, cherry [][]int) [][][2]int {
 	}
 }
 
-func secondPath(x, y, target int, cherry [][]int) int {
-	length := len(cherry)
-	if cherry[x][y] == -1 || target >= (length)*2-1-x-y {
+func secondPath(x, y, target int, cherry *Cherry) int {
+	length := len(cherry.Grid)
+	if cherry.Grid[x][y][0] || target >= (length)*2-1-x-y {
 		return -1
 	} else {
-		res := cherry[x][y]
+		var res int
+		if cherry.Grid[x][y][1] {
+			res = 1
+		} else {
+			res = 0
+		}
 		target -= res
 		if x == length-1 && y == length-1 {
 			return res
 		} else {
 			//！！！！！必须要给予xMove/yMove初始值-1！！！！！
 			xMove, yMove := -1, -1
-			if x != length-1 {
+			if x != length-1 && !cherry.Grid[x+1][y][0] {
 				xMove = secondPath(x+1, y, target, cherry)
 			}
-			if y != length-1 {
+			if y != length-1 && !cherry.Grid[x][y+1][0] {
 				yMove = secondPath(x, y+1, target, cherry)
 			}
 			if xMove == -1 && yMove == -1 {
@@ -62,21 +130,22 @@ func secondPath(x, y, target int, cherry [][]int) int {
 	}
 }
 
-func CherryPickup(cherry [][]int) int {
+func CherryPickup(cherryInt [][]int) int {
 	total := 0
 	fmt.Printf("Start\n")
+	cherry := new(Cherry)
+	cherry.transform(cherryInt)
 	paths := firstPath(0, 0, cherry)
 	fmt.Printf("Find all paths\n")
 	for p := 0; p < len(paths); p++ {
 		first := 0
-		grid := make([][]int, len(cherry))
-		for i := 0; i < len(cherry); i++ {
-			grid[i] = make([]int, len(cherry[i]))
-			copy(grid[i], cherry[i])
-		}
+		grid := new(Cherry)
+		grid.copyFrom(cherry)
 		for i := 0; i < len(paths[p]); i++ {
-			first += grid[paths[p][i][0]][paths[p][i][1]]
-			grid[paths[p][i][0]][paths[p][i][1]] = 0
+			if grid.Grid[paths[p][i][0]][paths[p][i][1]][1] {
+				first++
+			}
+			grid.Grid[paths[p][i][0]][paths[p][i][1]] = [2]bool{false, false}
 		}
 		if first < total>>1 {
 			continue
