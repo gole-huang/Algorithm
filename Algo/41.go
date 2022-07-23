15e3fb1e6932ddd38c7f2a20c2d147f7d572ce41
gole_huang <gole_huang@sohu.com>
2022-07-23 22:59:01 +0800

    add 41

--- Algo/41.go
+++ Algo/41.go
@@ -1,22 +1,19 @@
 package Algo
 
-/*
 func FirstMissingPositive(nums []int) int {
-	head, tail := 0, len(nums)-1
-	for head < tail { //让所有正整数在前，非正整数在后
-		for nums[head] < len(nums) && nums[head] > 0 {
-			head++
-		}
-		for nums[tail] >= len(nums) || nums[tail] <= 0 {
-			tail--
-		}
-		if (nums[head] >= len(nums) || nums[head] <= 0) && (nums[tail] < len(nums) && nums[tail] > 0) {
-			nums[head], nums[tail] = nums[tail], nums[head]
-			head++
-			tail--
+	//nums = append([]int{0}, nums...)
+	for i := 0; i < len(nums); i++ {
+		if nums[i] >= 1 && nums[i] <= len(nums) {
+			if nums[i] != i+1 && nums[i] != nums[nums[i]-1] {
+				nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+				i--
+			}
 		}
 	}
-	cnt := head //有cnt个
-	newNum := nums[:tail]
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != i+1 {
+			return i + 1
+		}
+	}
+	return len(nums) + 1
 }
-*/
