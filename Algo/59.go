bd368459772c82557659f501c9dc36f005c24904
gole_huang <gole_huang@sohu.com>
2022-07-30 00:49:31 +0800

    leedcode 30 not finished, 59 finished

---
+++ Algo/59.go
@@ -0,0 +1,22 @@
+package Algo
+
+func GenerateMatrix(n int) [][]int {
+	res := make([][]int, n)
+	for i := 0; i < n; i++ {
+		res[i] = make([]int, n)
+	}
+	val := 1
+	for i := 0; i < (n+1)/2; i++ {
+		for j := i; j < n-1-i; j++ {
+			res[i][j] = val + j - i
+			res[j][n-1-i] = val + n - 1 - 3*i + j
+			res[n-1-i][n-1-j] = val + 2*n - 2 - 5*i + j
+			res[n-1-j][i] = val + 3*n - 3 - 7*i + j
+		}
+		val += 4 * (n - 2*i - 1)
+	}
+	if n%2 != 0 {
+		res[n/2][n/2] = n * n
+	}
+	return res
+}
