22a4f055b24aae7577b46a8a89c067b5b4b45352
gole huang <gole.huang@gmail.com>
2022-08-13 23:09:45 +0800

    leedcode 69 almost perfect

---
+++ Algo/69.go
@@ -0,0 +1,21 @@
+package Algo
+
+func MySqrt(x int) int {
+	res := x
+	for res*res > x {
+		res = (res + x/res) >> 1
+	}
+	return res
+}
+
+/*	magic number: 0x5f375a86
+float InvSqrt(float x)
+{
+	float xhalf = 0.5f * x;
+	int i = *(int *)&x;
+	i = 0x5f3759df - (i>>1);
+	x = *(float *)&i;
+	x = x * (1.5f - xhalf * x * x);
+	return x;
+}
+*/
