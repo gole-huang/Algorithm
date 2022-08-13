383ca846d57af3db84efc85174084ab335bdfe4a
gole_huang <gole_huang@sohu.com>
2022-08-13 10:54:18 +0800

    leedcode 768 new finished

--- Algo/768.go
+++ Algo/768.go
@@ -1,18 +1,28 @@
 package Algo
 
-func addNum(array []int, num int) []int {
+func addNum(array [][]int, num int) [][]int {
 	for i := 0; i < len(array); i++ {
-		if num < array[i] {
+		if num < array[i][0] {
+			array[len(array)-1][1] = 1
 			return append(array[:i], array[len(array)-1])
 		}
 	}
-	return append(array, num)
+	if num == array[len(array)-1][0] {
+		array[len(array)-1][1]++
+	} else {
+		array = append(array, []int{num, 1})
+	}
+	return array
 }
 
-func maxChunksToSorted(arr []int) int {
-	res := []int{arr[0]}
+func MaxChunksToSorted(arr []int) int {
+	res := [][]int{{arr[0], 1}}
 	for i := 1; i < len(arr); i++ {
 		res = addNum(res, arr[i])
 	}
-	return len(res)
+	sum := 0
+	for _, v := range res {
+		sum += v[1]
+	}
+	return sum
 }
