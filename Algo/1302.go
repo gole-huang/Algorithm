a621353344166f3ecd6a4959332ac475671e56dd
gole huang <gole.huang@gmail.com>
2022-08-17 00:31:44 +0800

    leedcode 72 finished

---
+++ Algo/1302.go
@@ -0,0 +1,32 @@
+package Algo
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func DeepestLeavesSum(root *TreeNode) int {
+	nodes := []*TreeNode{root}
+	for len(nodes) != 0 {
+		tmpNodes := make([]*TreeNode, 0)
+		for _, v := range nodes {
+			if v.Left != nil {
+				tmpNodes = append(tmpNodes, v.Left)
+			}
+			if v.Right != nil {
+				tmpNodes = append(tmpNodes, v.Right)
+			}
+		}
+		if len(tmpNodes) != 0 {
+			nodes = tmpNodes
+		} else {
+			res := 0
+			for _, v := range nodes {
+				res += v.Val
+			}
+			return res
+		}
+	}
+	return root.Val
+}
---
+++ Algo/72.go
@@ -0,0 +1,48 @@
+package Algo
+
+func findChar(s string, c byte) int {
+	for i := 0; i < len(s); i++ {
+		if s[i] == c {
+			return i
+		}
+	}
+	return -1
+}
+
+func MinDistance(src, dst string) int {
+	if len(src) > len(dst) {
+		src, dst = dst, src
+	}
+	if src == "" {
+		return len(dst)
+	}
+	if len(src) == 1 {
+		if findChar(dst, src[0]) == -1 {
+			return len(dst)
+		}
+		return len(dst) - 1
+	}
+	if dst[0] == src[0] {
+		return MinDistance(dst[1:], src[1:])
+	}
+	min := len(dst)
+	if len(src) > len(dst) {
+		min = len(src)
+	}
+	// 匹配首字符
+	v := findChar(dst, src[0])
+	if v != -1 {
+		if min > MinDistance(src[1:], dst[v+1:])+v {
+			min = MinDistance(src[1:], dst[v+1:]) + v
+		}
+	}
+	// 删除首字符
+	if src[1] == dst[0] && min > MinDistance(src[2:], dst[1:])+1 {
+		min = MinDistance(src[2:], dst[1:]) + 1
+	}
+	// 更改
+	if min > MinDistance(src[1:], dst[1:])+1 {
+		min = MinDistance(src[1:], dst[1:]) + 1
+	}
+	return min
+}
