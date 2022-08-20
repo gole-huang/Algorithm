7dc1788f476f120cdf0601040688e71a2ac82ffb
gole huang <gole.huang@gmail.com>
2022-08-21 01:16:45 +0800

    leedcode 76 finished

--- Algo/76.go
+++ Algo/76.go
@@ -1,6 +1,10 @@
 package Algo
 
-func checkMap(m map[byte][2]int) bool {
+import (
+	"strings"
+)
+
+func match(m map[byte][]int) bool {
 	for _, v := range m {
 		if v[0] > v[1] {
 			return false
@@ -9,67 +13,86 @@ func checkMap(m map[byte][2]int) bool {
 	return true
 }
 
-/*
-	func replaceChar(pre, cur byte, m map[byte][2]int) bool {
-		m[pre] = [2]int{m[pre][0], m[pre][1] - 1}
-		m[cur] = [2]int{m[cur][0], m[cur][1] + 1}
-		return checkMap(m)
-	}
-*/
 func MinWindow(s string, t string) string {
 	if len(s) == 0 || len(t) == 0 || len(s) < len(t) {
 		return ""
 	} else if len(t) == 1 {
-		for k := range s {
-			if t[0] == s[k] {
-				return t
-			}
+		if strings.Contains(s, t) {
+			return t
 		}
 		return ""
 	}
-	tMap := map[byte][2]int{} //初始化map
+	// 初始化map
+	strMap := map[byte][]int{}
 	for k := range t {
-		if _, ok := tMap[t[k]]; !ok {
-			tMap[t[k]] = [2]int{1, 0}
+		if _, ok := strMap[t[k]]; !ok {
+			strMap[t[k]] = []int{1, 0}
 		} else {
-			tMap[t[k]] = [2]int{tMap[t[k]][0] + 1, tMap[t[k]][1]}
+			strMap[t[k]][0]++
 		}
 	}
+	// 双指针
 	res := [2]int{}
-	left, right := -1, 0 //双指针
-	loop := false
-	for right < len(s) {
-		if loop { //左指针前移，遇到重复字符，一直前移到map[0]
-			for left < right {
-				if _, ok := tMap[s[left]]; ok {
-					tMap[s[left]] = [2]int{tMap[s[left]][0], tMap[s[left]][1] - 1}
-					if !checkMap(tMap) {
-						break
-					} else {
-						if res[1]-res[0] > right-left {
-							res = [2]int{left, right}
-						}
-					}
-				}
-				left++
-			}
-		}
-		if _, ok := tMap[s[right]]; ok {
+	left, right := -1, 0
+	for right < len(s) { // 右指针单独前移，取第一个完整子字符串
+		if _, ok := strMap[s[right]]; ok {
 			if left == -1 {
 				left = right
 			}
-			tMap[s[right]] = [2]int{tMap[s[right]][0], tMap[s[right]][1] + 1}
-			if checkMap(tMap) {
-				loop = true
+			strMap[s[right]][1]++
+			if match(strMap) {
 				if res[0] == res[1] || res[1]-res[0] > right-left {
-					res = [2]int{left, right + 1}
+					res = [2]int{left, right}
 				}
+				break
 			}
 		}
+		right++
 	}
-	if res[0] == res[1] {
+	if left == -1 || right == len(s) {
 		return ""
-	} else {
-		return s[res[0] : res[1]+1]
 	}
+	for left < right {
+		if _, ok := strMap[s[left]]; ok {
+			strMap[s[left]][1]--
+		}
+		left++
+		if !match(strMap) {
+			left--
+			strMap[s[left]][1]++
+			break
+		}
+	}
+	res = [2]int{left, right}
+	for {
+		// 左右指针同时前移
+		right++
+		if right == len(s) {
+			break
+		}
+		if _, ok := strMap[s[right]]; ok {
+			strMap[s[right]][1]++
+		}
+		if _, ok := strMap[s[left]]; ok {
+			strMap[s[left]][1]--
+		}
+		left++
+		for match(strMap) {
+			v, ok := strMap[s[left]]
+			if !ok {
+				left++
+				continue
+			} else if v[0] < v[1] {
+				strMap[s[left]][1]--
+				left++
+				continue
+			} else {
+				break
+			}
+		}
+		if match(strMap) && right-left < res[1]-res[0] {
+			res = [2]int{left, right}
+		}
+	}
+	return s[res[0] : res[1]+1]
 }
