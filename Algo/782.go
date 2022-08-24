5a32504283b70a4606a5f8b2d0a44b72cdadf838
gole huang <gole.huang@gmail.com>
2022-08-24 23:25:11 +0800

    leedcode 782 finished

--- Algo/782.go
+++ Algo/782.go
@@ -1,15 +1,8 @@
 package Algo
 
-/*
-第一行作为比较，之后所有行，要么与第一行一样，要么与第一行相反。列也一样。
-第一行中，0的个数和1的个数相差不超过1.同样，第一列中0的个数和1的个数不超过1
-若第一行为偶数，则只需要计算出出现连续1的长度和次数。长度/2代表移动1列，把所有长度加起来即为待调整的次数。列也要单独算
-若第一行为奇数，则需要判断0和1较多者是否在两个角落，否则每个角落需要额外+1
-*/
+// 第一行作为比较，之后所有行，要么与第一行一样，要么与第一行相反。
+// 列也一样。
 func compare(a, b []int) bool {
-	if len(a) != len(b) {
-		return false
-	}
 	reverse := a[0] != b[0]
 	for k := range a {
 		if reverse && a[k] == b[k] || !reverse && a[k] != b[k] {
@@ -20,123 +13,82 @@ func compare(a, b []int) bool {
 }
 
 func isChessboard(brd [][]int) (bool, int) {
-	zero, one := 0, 0   // 首行、首列的0、1个数
-	con0, con1 := 1, 1  // 每一个连续0，1的长度
-	zeros, ones := 0, 0 // 首行、首列连续0、1中需要移动的次数
-	var cnt int         //交换总数
-	//首行
-	for i := 0; i < len(brd); i++ {
-		if brd[0][i] == 0 {
-			zero++
-			if i > 0 {
-				if brd[0][i-1] == 0 {
-					con0++
-				} else if con1 > 1 {
-					ones += con1 >> 1
-					con1 = 1
-				}
-			}
-		} else {
-			one++
-			if i > 0 {
-				if brd[0][i-1] == 1 {
-					con1++
-				} else {
-					zeros += con0 >> 1
-					con0 = 1
-				}
-			}
-		}
+	_0to0, _0to1, _1to0, _1to1 := 0, 0, 0, 0
+	var cnt int // 交换总数
 
-	}
-	if len(brd)%2 == 0 {
-		if zero != one {
-			return false, -1
-		}
-		zeros += con0 >> 1
-		ones += con1 >> 1
-		cnt = max(zeros, ones)
-	} else if zero > one {
-		if zero-one != 1 {
-			return false, -1
-		}
-		if brd[0][0] != 0 {
-			zeros++
-		}
-		if brd[len(brd)-1][0] != 0 {
-			zeros++
-		}
-		cnt = zeros
-	} else {
-		if one-zero != 1 {
-			return false, -1
-		}
-		if brd[0][0] != 1 {
-			ones++
-		}
-		if brd[len(brd)-1][0] != 0 {
-			ones++
-		}
-		cnt = ones
-	}
-	zeros, one = 0, 0
-	for i := 0; i < len(brd); i++ {
-		if brd[i][0] == 0 {
-			zero++
-			if i > 0 {
-				if brd[0][i-1] == 0 {
-					con0++
-				} else if con1 > 1 {
-					ones += con1 >> 1
-					con1 = 1
-				}
+	for i := range brd[0] { // 首行
+		if brd[0][i] == 0 {
+			if i%2 != 0 { // 假设棋盘首行为0101010101......，不是也无所谓，比较每一位与该位置棋盘该有的数字做比较。
+				_0to1++ // 若应该为0（i%2==0），而此处为1，则_0to1++
+			} else {
+				_0to0++ // 若应该为0（i%2==0），而此处为0，则_0to0++
 			}
 		} else {
-			one++
-			if i > 0 {
-				if brd[0][i-1] == 1 {
-					con1++
-				} else {
-					zeros += con0 >> 1
-					con0 = 1
-				}
+			if i%2 != 1 {
+				_1to0++ // 若应该为0（i%2==1），而此处为1，则_1to0++
+			} else {
+				_1to1++ // 若应该为1（i%2==1），而此处为1，则_1to1++
+			}
+		}
+	}
+	if len(brd)%2 == 0 { // 若n为偶数，则0到1的转换应该和1到0的转换一样。如果棋盘是10101010......，则0到0的转换应该等于1到1的转换
+		if _0to1 != _1to0 || _0to0 != _1to1 {
+			return false, -1
+		}
+		if _0to1+_1to0 > _0to0+_1to1 {
+			cnt += _0to0
+		} else {
+			cnt += _0to1
+		}
+	} else { //若n为偶数，则情况比较复杂。要么0到1的转换等于1到0的转换，且0到0的转换和1到1的转换之差不超过1，或者相反
+		if (_0to1 != _1to0 || (_0to0-_1to1 != 1 && _0to0-_1to1 != -1)) && (_0to0 != _1to1 || (_0to1-_1to0 != 1 && _0to1-_1to0 != -1)) {
+			return false, -1
+		}
+		if _0to1 != _1to0 {
+			cnt += _0to0
+		} else {
+			cnt += _0to1
+		}
+	}
+	_0to0, _0to1, _1to0, _1to1 = 0, 0, 0, 0
+	for i := range brd { // 首列
+		if brd[i][0] == 0 {
+			if i%2 != 0 {
+				_0to1++
+			} else {
+				_0to0++
+			}
+		} else {
+			if i%2 != 1 {
+				_1to0++
+			} else {
+				_1to1++
 			}
 		}
 	}
 	if len(brd)%2 == 0 {
-		if zero != one {
+		if _0to1 != _1to0 || _0to0 != _1to1 {
 			return false, -1
 		}
-		zeros += con0 >> 1
-		ones += con1 >> 1
-		cnt += max(zeros, ones)
-	} else if zero > one {
-		if zero-one != 1 {
-			return false, -1
+		if _0to1+_1to0 > _0to0+_1to1 {
+			cnt += _0to0
+		} else {
+			cnt += _0to1
 		}
-		if brd[0][0] != 0 {
-			zeros++
-		}
-		if brd[len(brd)-1][0] != 0 {
-			zeros++
-		}
-		cnt += zeros
 	} else {
-		if one-zero != 1 {
+		if (_0to1 != _1to0 || (_0to0-_1to1 != 1 && _0to0-_1to1 != -1)) && (_0to0 != _1to1 || (_0to1-_1to0 != 1 && _0to1-_1to0 != -1)) {
 			return false, -1
 		}
-		if brd[0][0] != 1 {
-			ones++
+		if _0to1 != _1to0 {
+			cnt += _0to0
+		} else {
+			cnt += _0to1
 		}
-		if brd[len(brd)-1][0] != 0 {
-			ones++
-		}
-		cnt += ones
 	}
-	return true, max(zeros, ones)
+	return true, cnt
 }
 
-func movesToChessboard(board [][]int) int {
+func MovesToChessboard(board [][]int) int {
 	firstCheck, res := isChessboard(board)
 	if !firstCheck {
 		return -1
@@ -146,4 +98,13 @@ func movesToChessboard(board [][]int) int {
 			return -1
 		}
 	}
+	for i := 1; i < len(board); i++ {
+		reverse := board[i][0] != board[0][0]
+		for k := range board[i] {
+			if reverse && board[i][k] == board[0][k] || !reverse && board[i][k] != board[0][k] {
+				return -1
+			}
+		}
+	}
+	return res
 }
