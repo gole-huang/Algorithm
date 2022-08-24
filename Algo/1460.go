7f47921963a31c270c421e07d33a7aee4e502d51
gole huang <gole.huang@gmail.com>
2022-08-24 23:29:03 +0800

    leedcode 1460 finished

---
+++ Algo/1460.go
@@ -0,0 +1,19 @@
+package Algo
+
+func CanBeEqual(target []int, arr []int) bool {
+	for len(target) > 0 {
+		nomatch := true
+		for j := 0; j < len(arr); j++ {
+			if target[0] == arr[j] {
+				target = target[1:]
+				arr = append(arr[:j], arr[j+1:]...)
+				nomatch = false
+				break
+			}
+		}
+		if nomatch {
+			return false
+		}
+	}
+	return true
+}
