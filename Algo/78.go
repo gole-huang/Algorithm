007db1f8d9afe491b4ca51a365a8fe09542ef7cf
gole-huang <gole.huang@gmail.com>
2022-08-29 13:27:29 +0800

    78 finished

---
+++ Algo/78.go
@@ -0,0 +1,18 @@
+package Algo
+
+func Subsets(nums []int) [][]int {
+	if len(nums) == 1 {
+		return [][]int{{}, nums}
+	} else {
+		arr := Subsets(nums[1:])
+		res := make([][]int, 0)
+		for _, v := range arr {
+			res = append(res, v)
+			k := make([]int, len(v)+1)
+			copy(k, v)
+			k[len(k)-1] = nums[0]
+			res = append(res, k)
+		}
+		return res
+	}
+}
