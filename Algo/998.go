28f7aed71e59de0a04a1fad51b8c89ef32e893b9
gole huang <gole.huang@gmail.com>
2022-08-30 00:31:57 +0800

    leedcode 998 finished perfectly

---
+++ Algo/998.go
@@ -0,0 +1,21 @@
+package Algo
+
+func InsertIntoMaxTree(root *TreeNode, val int) *TreeNode {
+	if val > root.Val {
+		head := new(TreeNode)
+		head.Val = val
+		head.Left = root
+		return head
+	}
+	cur := root
+	node := new(TreeNode)
+	node.Val = val
+	for cur.Right != nil && cur.Right.Val > val {
+		cur = cur.Right
+	}
+	if cur.Right != nil {
+		node.Left = cur.Right
+	}
+	cur.Right = node
+	return root
+}
