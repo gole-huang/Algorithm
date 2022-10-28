f5d90cc26f0b549c5d48506e0e0882dbd692830f
gole huang <gole.huang@gmail.com>
2022-10-29 01:02:10 +0800

    64 finished

--- Algo/64.go
+++ Algo/64.go
@@ -1,30 +1,25 @@
 package algo
 
-func pathSum(x, y int, grid [][]int) int {
-	if x == len(grid)-1 && y == len(grid[x])-1 {
-		return grid[x][y]
-	} else if x == len(grid)-1 {
-		res := grid[x][y]
-		for i := 1; i < len(grid[x])-y; i++ {
-			res += grid[x][y+i]
-		}
-		return res
-	} else if y == len(grid[x])-1 {
-		res := grid[x][y]
-		for i := 1; i < len(grid)-x; i++ {
-			res += grid[x+i][y]
-		}
-		return res
+func min(a, b int) int {
+	if a < b {
+		return a
 	} else {
-		res1, res2 := pathSum(x+1, y, grid), pathSum(x, y+1, grid)
-		if res1 > res2 {
-			return res2
-		} else {
-			return res1
-		}
+		return b
 	}
 }
-
 func MinPathSum(grid [][]int) int {
-	return pathSum(0, 0, grid)
+	for km := range grid {
+		for kn := range grid[km] {
+			if km == 0 && kn == 0 {
+				continue
+			} else if km == 0 {
+				grid[0][kn] += grid[0][kn-1]
+			} else if kn == 0 {
+				grid[km][0] += grid[km-1][0]
+			} else {
+				grid[km][kn] += min(grid[km-1][kn], grid[km][kn-1])
+			}
+		}
+	}
+	return grid[len(grid)-1][len(grid[0])-1]
 }
